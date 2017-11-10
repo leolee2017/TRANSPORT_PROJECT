8ce2f55cf9e69e596ca0a9afc44e94119f834a74
Leo <leolee85267@gmail.com>
2017-11-10 17:38:39 +0800

    commit removed files

--- website/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $('.member-card').mouseenter(() => {
-        $(event.currentTarget).find('.more').css('text-decoration', 'underline');
-    });
-    $('.member-card').mouseleave(() => {
-        $(event.currentTarget).find('.more').css('text-decoration', 'none');
-    });
-});
